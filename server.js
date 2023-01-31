f227b9b6e183f62414ebfd485db7368acba4423e
Alex Manet <alexander.manap@gmail.com>
2023-01-31 09:32:03 +0100

    página de lectura de cookies

--- server.js
+++ server.js
@@ -10,11 +10,22 @@ app.get('/', (req, res) => {
 })
 
 app.get('/setcookie', (req, res) => {
-    res.cookie('moto', 'mami')
+    res.cookie('moto', 'mami', {
+        //maxAge: 7000,
+        expires: new Date('2023-12-31'),
+        httpOnly: true,
+        secure: true,
+        sameSite: 'lax'
+        
+    })
 
     res.send('Pagina principal de manejo de cookies')
 })
 
+app.get('/getcookie', (req, res) => {
+    console.log(req.cookies)
+    res.send('Reading cookies...')
+})
 
 app.listen(3000)
 console.log('Server en el puerto 3000')
